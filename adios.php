02be2f208ae493e9f773639a13cb26404e213c6f
FranciscoCastilloCastillo <fmanuelcastillo@hotmail.com>
2017-11-16 17:50:35 +0100

    Estamos haciendo commit del proyecto php

---
+++ adios.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Nuestro primer html</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  </head>
+  <body style="margin-left:15px; margin-right:15px">
+    <header>
+      <h2>CASO PRÁCTICO 1 – REFORZANDO HTML</h2>
+    </header>
+    <article style="margin-top:50px">
+      <?php echo "<p style='color:red'>Hasta luego desarrollador!!!</p>" ?>
+    </article>
+    <footer style="margin-top:100px; background-color:grey; color:white">
+      <h5>Alumno: Francisco Manuel Castillo Castillo</h5>
+      <h5>Asignatura: Desarrollo Web Entorno Servidor</h5>
+    </footer>
+  </body>
+</html>
